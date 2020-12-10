aedbe71936fe5063471dd1160f7d4e7154c1230a
Bisri Mustofa <57256951+Bisri88@users.noreply.github.com>
2020-12-10 18:43:43 +0700

    Add files via upload

---
+++ Mikrokontroller/pin_PWM_analogWrite_IoT_Blynk.ino
@@ -0,0 +1,33 @@
+#define BLYNK_PRINT Serial
+#include <ESP8266WiFi.h>
+#include <BlynkSimpleEsp8266.h>
+int led1 = D1;
+int led2 = D4;
+int led3 = D5;
+int led4 = D7;
+char auth[] = "Auth Dari Blynk App";
+char ssid[] = "SSID";
+char pass[] = "Password";
+void setup() {
+  // put your setup code here, to run once:
+Blynk.begin(auth,ssid,pass);
+Serial.begin(9600);
+pinMode(led1, OUTPUT);
+pinMode(led2, OUTPUT);
+pinMode(led3, OUTPUT);
+pinMode(led4, OUTPUT);
+}
+BLYNK_WRITE(V1)
+{
+  nilai = param.asInt();
+  analogWrite(led1, nilai);
+  analogWrite(led2, nilai);
+  analogWrite(led3, nilai);
+  analogWrite(led4, nilai);
+  Blynk.virtualWrite(V2, nilai);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+Blynk.run();
+}
