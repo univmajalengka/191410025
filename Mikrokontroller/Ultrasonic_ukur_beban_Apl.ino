51843e68da343219e2d8aaaa3465d27bb112c63c
Bisri Mustofa <57256951+Bisri88@users.noreply.github.com>
2021-01-08 17:59:07 +0700

    Update Ultrasonic_ukur_beban_Apl.ino

--- Mikrokontroller/Ultrasonic_ukur_beban_Apl.ino
+++ Mikrokontroller/Ultrasonic_ukur_beban_Apl.ino
@@ -1,6 +1,5 @@
 const int trig = D7;
 const int echo = D8;
-//int ht = 16 ;
 long duration,mm,mm1;
 float gr;
 int gram;
@@ -9,8 +8,6 @@ void setup() {
 pinMode(trig, OUTPUT);
 pinMode(echo, INPUT);
 Serial.begin(9600);
-//lcd.begin(16,2);
-//ht = 16;
 }
 
 void loop() {
@@ -32,7 +29,7 @@ void ultrasonic()
   mm1 = mm-20;//Jarak Terdekat Ultrasonic dalam Pengukuran Awal 2 cm atau 20 mm
   //Jadi Kalau MIsalkan Jarak 20 mm,, Maka Itu Akan Dinilai 0 Gram,,
   gr = mm1 * 0.5;//tetapan Pegas Apabila Bertambah 1 mili Maka Bertambah 0.5 gram(dikalikan dengan 0.5)
-  gram = gr;
+  gram = gr;//Konversi Float ke Integer(Karena GUI Di Desktop Aplication tidak Bisa Menerima Value Desimal,,
   Serial.print("H");
   Serial.println(mm);
   Serial.print("T");
